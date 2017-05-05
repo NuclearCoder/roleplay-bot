4e99c37ef71bbf6e4791161a4c4c75c7ac70b553
choco1 <choco1@users.noreply.github.com>
2017-05-05 18:44:28 +1200

    Removed old D4J stuff and added image sending and downloading with InputStream and java.io.url

--- src/main/kotlin/org/kud/roleplay/util/Discord.kt
+++ src/main/kotlin/org/kud/roleplay/util/Discord.kt
@@ -1,36 +1,39 @@
 package org.kud.roleplay.util
 
+import net.dv8tion.jda.core.MessageBuilder
 import net.dv8tion.jda.core.entities.Member
+import net.dv8tion.jda.core.entities.TextChannel
+import org.kud.roleplay.LOGGER
+import org.kud.roleplay.URL_GET_TIMEOUT
+import java.io.IOException
+import java.io.InputStream
+import java.net.URL
 
-/*fun IChannel.rSendFileURL(content: String, url: String,
-                          filename: String) {
+fun TextChannel.sendImagefromURL(content: String, url: String,
+                                 filename: String) {
     try {
-        val conn = URL(url).openConnection()
-        conn.doInput = true
-        conn.doOutput = true
-        conn.readTimeout = URL_GET_TIMEOUT
-        conn.connectTimeout = URL_GET_TIMEOUT
-
-        conn.setRequestProperty("User-Agent",
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11")
-
-        conn.inputStream.use {
-            RequestBuffer.request {
-                try {
-                    sendFile(content, false, it, filename)
-                } catch (e: MissingPermissionsException) {
-                    LOGGER.error("Could not send file", e)
-                } catch (e: DiscordException) {
-                    LOGGER.error("Could not send file", e)
-                }
-            }
+        getInputStreamFromUrl(url).use {
+            this.sendFile(it, filename, MessageBuilder().append(content).build()).queue()
         }
     } catch (e: IOException) {
         LOGGER.error("Could not send file:", e)
     }
 
 }
-*/
+
+fun getInputStreamFromUrl(url: String): InputStream {
+    val conn = URL(url).openConnection()
+    conn.doInput = true
+    conn.doOutput = true
+    conn.readTimeout = URL_GET_TIMEOUT
+    conn.connectTimeout = URL_GET_TIMEOUT
+
+    conn.setRequestProperty("User-Agent",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11")
+
+    return conn.getInputStream()
+}
+
 fun Member.hasRoleForGuild(roleID: Long): Boolean {
     return this.roles.any { it.idLong == roleID }
 }
