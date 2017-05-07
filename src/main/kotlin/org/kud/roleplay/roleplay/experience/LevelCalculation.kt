8897d0caa79d36858316745b8af62b46e0484cc8
NuclearCoder <hd85.ms@gmail.com>
2017-05-07 02:17:00 +0200

    Added some documentation to LevelCalculation

--- src/main/kotlin/org/kud/roleplay/roleplay/experience/LevelCalculation.kt
+++ src/main/kotlin/org/kud/roleplay/roleplay/experience/LevelCalculation.kt
@@ -1,10 +1,14 @@
 package org.kud.roleplay.roleplay.experience
 
+// from this level, level values grow by a constant amount
 private val levelThreshold = 25
 
+// affine exponential growth, slightly below 2^n
+// this holds the value of each level on its own.
 private val levelValues = LongArray(levelThreshold) { 500 + Math.pow(1.8, it.toDouble()).toLong() }
-private val levelThresholdValue = levelValues.last()
+private val levelThresholdValue = levelValues.last() // this is the value of the last level before affine growth
 
+// this holds the total experience needed to reach each level (sum of all preceeding values)
 private val levelExperience = LongArray(levelThreshold).apply {
     this[0] = levelValues[0]
     (1..levelThreshold - 1).forEach {
@@ -13,16 +17,24 @@ private val levelExperience = LongArray(levelThreshold).apply {
 }
 private val levelThresholdExperience = levelExperience.last()
 
+/**
+ * Returns the total experience needed to reach a level.
+ */
 fun getLevelExperience(level: Int) =
         levelExperience[level.coerceAtMost(levelThreshold) - 1] +
                 (level - levelThreshold).coerceAtLeast(0) * levelThresholdValue
 
-fun getLevelFor(exp: Long): Pair<Int, Long> {
-    val level = if (exp > levelThresholdExperience) {
-        25 + ((exp - levelThresholdExperience) / levelThresholdValue).toInt()
-    } else {
-        (25 downTo 1).first { exp > levelExperience[it] }
-    }
-    val levelXP = exp - getLevelExperience(level)
-    return Pair(level, levelXP)
-}
+/**
+ * Returns the level and current level experience for the given total experience.
+ */
+fun getLevelFor(exp: Long): Pair<Int, Long> =
+        if (exp > levelThresholdExperience) {
+            25 + ((exp - levelThresholdExperience) / levelThresholdValue).toInt()
+        } else {
+            (25 downTo 1).first { exp > levelExperience[it] }
+        }.let { level ->
+            (exp - getLevelExperience(level)).let {
+                levelXP ->
+                Pair(level, levelXP)
+            }
+        }
