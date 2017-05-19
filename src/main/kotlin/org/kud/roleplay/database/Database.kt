df6cc427ffc75ef0cddefee7ad30f5c107c2af90
NuclearCoder <hd85.ms@gmail.com>
2017-05-19 22:30:28 +0200

    Using MySQL data source for fine tuned connection

--- src/main/kotlin/org/kud/roleplay/database/Database.kt
+++ src/main/kotlin/org/kud/roleplay/database/Database.kt
@@ -1,22 +1,25 @@
 package org.kud.roleplay.database
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.kud.roleplay.util.Config
 
+
 class Database(config: Config) {
 
-    private val host = config["database_host"]
-    private val port = config["database_port"]
-    private val schema = config["database_schema"]
-
-    private val user = config["database_user"]
-    private val pass = config["database_pass"]
-
     init {
-        Database.connect("jdbc:mysql://$host:$port/$schema", driver = "com.mysql.jdbc.Driver",
-                user = user, password = pass)
+        Database.connect(MysqlDataSource().apply {
+            serverName = config["database_host"]
+            port = config["database_port"].toInt()
+            useSSL = false
+
+            databaseName = config["database_schema"]
+
+            user = config["database_user"]
+            setPassword(config["database_pass"])
+        })
 
         transaction {
             create(Guilds, Users, Characters)
